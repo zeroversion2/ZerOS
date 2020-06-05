6982e4aeebf256864ffa6b3772dcb4dfd20d5057
Zero <zerotwofan@gmail.com>
2020-06-04 22:33:39 -0700

    fix inportb return

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -9,6 +9,7 @@
 unsigned char inportb(unsigned short _port) {
     unsigned char rv;
     asm volatile("inb %1, %0" : "=a" (rv) : "dN" (_port));
+    return rv;
 }
 
 void outportb (unsigned short _port, unsigned char _data) {
